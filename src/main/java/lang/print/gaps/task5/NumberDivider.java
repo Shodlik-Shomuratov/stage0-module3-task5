364fc2ffa489e1019e442c03e17f9c0fbf53c002
Shodlik Shomuratov <shodlikcoder@gmail.com>
2022-11-25 19:40:55 +0500

    All tasks done!

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float divided = toBeDivided / 5;
+
+        System.out.println(divided);
     }
 }
